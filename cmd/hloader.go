162c3a082e543108ddba578c71f11e09f6a8b630
noi <noidor2019@gmail.com>
2024-11-14 16:54:39 +0700

    First crude requests goroutine

--- cmd/hloader.go
+++ cmd/hloader.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/Noiidor/highloader/pkg/highloader"
+)
+
+func main() {
+	err := highloader.Run(highloader.AppArgs{
+		URL:         "http://127.0.0.1:5050/echo",
+		Method:      highloader.POST,
+		HTTPVersion: "1.1",
+		ReqTotal:    5000,
+		Payload:     "{test: \"test\"}",
+		ReqTimeout:  100,
+		Timeout:     100,
+	})
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	}
 }
--- pkg/highloader/highloader.go
+++ pkg/highloader/highloader.go
@@ -4,7 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -36,35 +40,103 @@ type AppArgs struct {
 	Timeout uint64 // seconds
 }
 
+// TODO: replace with not global vars
+var TotalRequests atomic.Uint64
+var SuccessfulRequests atomic.Uint64
+var FailedRequests atomic.Uint64
+var ErrorRequests atomic.Uint64
+
+var ErrorsChan = make(chan error, 5000)
+
 func Run(args AppArgs) error {
 	ctx, cancel := context.WithTimeout(context.TODO(), time.Second*time.Duration(args.Timeout))
 	defer cancel()
-
-	var body *bytes.Buffer
-	if args.Payload != nil {
-		err := json.NewEncoder(body).Encode(args.Payload)
-		if err != nil {
-			return err
-		}
-	}
-	req, err := http.NewRequestWithContext(ctx, args.Method.String(), args.URL, body)
-	if err != nil {
-		return err
-	}
-
-	for k, v := range args.Headers {
-		req.Header.Set(k, v)
-	}
 
 	client := http.Client{
 		Timeout: time.Millisecond * time.Duration(args.ReqTimeout),
 	}
 
-	res, err := client.Do(req)
-	if err != nil {
-		return err
+	wg := sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		for TotalRequests.Add(1) <= args.ReqTotal {
+
+			body := new(bytes.Buffer) // TODO: reuse?
+			if args.Payload != nil {
+
+				jsonData, err := json.Marshal(args.Payload)
+				if err != nil {
+					ErrorRequests.Add(1)
+					ErrorsChan <- err
+					continue
+				}
+
+				_, err = body.Write(jsonData)
+				if err != nil {
+					ErrorRequests.Add(1)
+					ErrorsChan <- err
+					continue
+				}
+			}
+			req, err := http.NewRequestWithContext(ctx, args.Method.String(), args.URL, body)
+			if err != nil {
+				ErrorRequests.Add(1)
+				ErrorsChan <- err
+				continue
+			}
+
+			for k, v := range args.Headers {
+				req.Header.Set(k, v)
+			}
+
+			res, err := client.Do(req)
+			if err != nil {
+				ErrorRequests.Add(1)
+				ErrorsChan <- err
+				continue
+			}
+			defer res.Body.Close()
+
+			if res.Status[0] == []byte("2")[0] {
+				SuccessfulRequests.Add(1)
+			} else {
+				FailedRequests.Add(1)
+			}
+
+			_, err = io.ReadAll(req.Body)
+			if err != nil {
+
+				ErrorRequests.Add(1)
+				ErrorsChan <- err
+				continue
+			}
+
+		}
+		close(ErrorsChan)
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		for TotalRequests.Load() < args.ReqTotal { // TODO: change to be dependent on requests goroutines
+			select {
+			case <-time.After(time.Second * 1):
+				fmt.Printf("Total Requests: %d\n", TotalRequests.Load())
+				fmt.Printf("Successful Requests: %d\n", SuccessfulRequests.Load())
+				fmt.Printf("Failed Requests: %d\n", FailedRequests.Load())
+				fmt.Printf("Error Requests: %d\n", ErrorRequests.Load())
+				fmt.Println("-----")
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	for v := range ErrorsChan {
+		fmt.Printf("Err: %s\n", v)
 	}
-	_ = res
 
 	return nil
 }
