c9a6d33943ac5a769b6e4cafa5ad95c136892325
noi <noidor2019@gmail.com>
2024-12-12 16:17:05 +0700

    Added tests and benchmarks, for real now

---
+++ pkg/highloader/highloader_test.go
@@ -0,0 +1,100 @@
+package highloader
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func BenchmarkRun100(b *testing.B)   { benchmarkRun(100, b) }
+func BenchmarkRun1000(b *testing.B)  { benchmarkRun(1000, b) }
+func BenchmarkRun10000(b *testing.B) { benchmarkRun(10000, b) }
+
+// Not sure if this is a good benchmark
+func benchmarkRun(n uint64, b *testing.B) {
+	ctx := context.Background()
+
+	// simplest echo server
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Body)
+	}))
+	defer server.Close()
+
+	pushFreq := 1 * time.Second
+
+	args := Opts{
+		URL:           server.URL,
+		Method:        HTTPMethodPOST,
+		HTTPVersion:   1,
+		Payload:       []byte("test_payload"),
+		RPS:           2 << 20,
+		ReqTotal:      n,
+		StatsPushFreq: pushFreq,
+	}
+
+	b.ResetTimer()
+	for range b.N {
+		_, _, err := Run(ctx, args)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+}
+
+func BenchmarkNewRequest(b *testing.B) {
+	ctx := context.Background()
+	for range b.N {
+		_, err := newRequest(ctx, "POST", "http://0.0.0.0:5050", bytes.NewReader([]byte("test_payload")), nil)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func TestRun(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Body)
+	}))
+	defer server.Close()
+
+	pushFreq := 50 * time.Millisecond
+
+	args := Opts{
+		URL:           server.URL,
+		Method:        HTTPMethodPOST,
+		HTTPVersion:   1,
+		Payload:       []byte("test_payload"),
+		RPS:           2 << 20,
+		ReqTotal:      50000,
+		StatsPushFreq: pushFreq,
+	}
+
+	stats, errs, err := Run(ctx, args)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case <-stats:
+	case <-time.After(pushFreq * 2):
+		t.Error("Expected non-zero number of stats, got 0")
+	}
+
+	for n := range errs {
+		t.Errorf("Expected no errors, got: %s", n.Error())
+	}
+	// select {
+	// case err := <-errs:
+	// 	t.Errorf("Expected none errors, got: %s", err.Error())
+	// case <-ctx.Done():
+	// 	break
+	// }
+}
